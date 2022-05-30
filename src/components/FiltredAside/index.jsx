ee5cf69abfd00da176c76a2effb823200793d146
Alex <5994708@mail.ru>
2022-05-30 22:52:35 +0300

    fix filter block

--- src/components/FiltredAside/index.jsx
+++ src/components/FiltredAside/index.jsx
@@ -2,18 +2,21 @@ import { Form } from "react-bootstrap"
 
 
 const FiltredAside = () => {
+    const filterItem = ['- 1 пересдка', '- без пересадок']
+
     return (
         <Form >
             <h6>Фильтровать</h6>
             <div className="mb-3">
-                <Form.Check
-                    type='checkbox'
-                    label='- 1 пересдка'
-                />
-                <Form.Check
-                    type='checkbox'
-                    label='- без пересадок'
-                />
+                {filterItem.map((item, index) => {
+                    return (
+                        <Form.Check
+                            key={index}
+                            type='checkbox'
+                            label={item}
+                        />
+                    )
+                })}
 
             </div>
 
